205ce1222690d65b6ae695e26a5a6c0a61df63bd
akio0911 <akio0911@gmail.com>
2021-11-13 20:35:52 +0900

    feedback

--- muranakaKadai14/FruitsTableViewController.swift
+++ muranakaKadai14/FruitsTableViewController.swift
@@ -11,16 +11,12 @@ class FruitsTableViewController: UITableViewController {
     private var fruits: [Fruit] =
         [
             Fruit(name: "りんご", isCheck: false),
-            Fruit(name: "みかん", isCheck: true ),
+            Fruit(name: "みかん", isCheck: true),
             Fruit(name: "バナナ", isCheck: false),
             Fruit(name: "パイナップル", isCheck: true)
         ]
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        1
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         fruits.count
@@ -39,16 +35,13 @@ class FruitsTableViewController: UITableViewController {
     @IBAction private func addFruit(segue: UIStoryboardSegue) {
         let addVC = segue.source as! AddFruitViewController  // swiftlint:disable:this force_cast
         guard let text = addVC.text else { return }
-        guard text != "" else { return }
+        guard !text.isEmpty else { return }
         fruits.append(Fruit(name: text, isCheck: false))
 
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-            [weak self] in
-            self!.tableView.insertRows(
-                at: [IndexPath.init(row: self!.fruits.count - 1, section: 0)],
-                with: .top
-            )
-        }
+        tableView.insertRows(
+            at: [IndexPath.init(row: fruits.count - 1, section: 0)],
+            with: .top
+        )
     }
 
     @IBAction private func cancel(segue: UIStoryboardSegue) {
